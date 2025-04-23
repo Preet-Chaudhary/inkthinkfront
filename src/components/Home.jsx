b46eb23e61ad0949e021ca30c729763a1b4061cc
Preet Chaudhary <preetchaudhary224@gmail.com>
2025-04-23 13:21:37 +0530

    all frontend

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faXmark } from '@fortawesome/free-solid-svg-icons';
+
+const Home = ({ onClose }) => {
+  return (
+    <div className="main-container-home">
+      <header className="home-header">
+        <FontAwesomeIcon icon={faHome} className="icon" />
+        <h1 className="home-h1">About Us</h1>
+        <FontAwesomeIcon icon={faXmark} className="icon" id="icon-cross" onClick={onClose} />
+      </header>
+      <p className="paragraph-home">
+      Ink & Think** is a real-time multiplayer drawing and guessing game inspired by Skribbl.io, developed by preet with ❤️.</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/News.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './News.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faEnvelopeOpen, faHome, faXmark } from '@fortawesome/free-solid-svg-icons';
+
+const Home = ({ onClose }) => {
+  return (
+    <div className="main-container-home">
+      <header className="home-header">
+        <FontAwesomeIcon icon={faEnvelope} className="icon" />
+        <h1 className="home-h1">News</h1>
+        <FontAwesomeIcon icon={faXmark} className="icon" id="icon-cross" onClick={onClose} />
+      </header>
+      <p className="paragraph-home">
+      Ink & Think** is a real-time multiplayer drawing and guessing game inspired by Skribbl.io, developed by preet with ❤️.</p>
+    </div>
+  );
+};
+
+export default Home;
